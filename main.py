d8389727f9ab5756a3296784131ef6b9d5acb336
kameil <racovikpvpbr@gmail.com>
2024-11-03 15:21:49 -0300

    frase mudada

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+import discord, random
 from discord.ext import commands
 
 bot = commands.Bot("p!", intents=discord.Intents.all(), help_command=None)
@@ -10,7 +10,7 @@ async def on_ready():
 async def on_message(message: discord.Message):
     if message.author.id == 716390085896962058 and "Please tell us you're human!" in message.content:
         await message.author.kick(reason="captcha nas area")
-        await message.channel.send("atividade para nao virar saudade")
+        await message.channel.send(random.choice(["a patrao eu nao consigo fazer captcha, q nao consegue rapaz segue o orea seca o estilo e esse."]))
     if "desenrolado" in message.content:
         await message.channel.send("tem que ser assim rapaz")
 
