ee257e1f285178dc048933e80e6c7d9ac38b300f
kameil <racovikpvpbr@gmail.com>
2024-11-03 18:26:24 -0300

    slash commands

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import discord, random
 from discord.ext import commands
+from discord import app_commands
 
 bot = commands.Bot("p!", intents=discord.Intents.all(), help_command=None)
 
 
 async def on_ready():
     print(f"Bot logado em {bot.user.name}")
+    comandos = await bot.tree.sync()
+    print(f"{len(comandos)} foram sincronizados.")
 
 async def on_message(message: discord.Message):
     if message.author.id == 716390085896962058 and "Please tell us you're human!" in message.content:
@@ -14,6 +17,10 @@ async def on_message(message: discord.Message):
     if "desenrolado" in message.content:
         await message.channel.send("tem que ser assim rapaz")
 
+
+@bot.tree.command("desenrolado")
+async def desenroladocmd(inter: discord.Interaction):
+    await inter.response.send_message("Tem q ser assim rapaz")
 
 bot.add_listener(on_message, 'on_message')
 bot.add_listener(on_ready, 'on_ready')
